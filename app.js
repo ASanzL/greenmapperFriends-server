c5cc77021aa34a88f764be800dc3a44fb52e2e3e
Andreas <andreas.sanz@hotmail.com>
2024-04-04 19:40:57 +0200

    Improved function to dont reuse calculations

--- app.js
+++ app.js
@@ -1,39 +1,41 @@
-const express = require('express');
-const { MongoClient } = require('mongodb');
-const cors = require('cors');
-const bodyparser = require('body-parser');
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const cors = require("cors");
+const bodyparser = require("body-parser");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
-app.use(bodyparser.json({limit: '50mb'}));
-const port = 3001
+app.use(bodyparser.json({ limit: "50mb" }));
+const port = 3001;
 
-const url = 'mongodb://127.0.0.1:27017';
+const url = "mongodb://127.0.0.1:27017";
 const client = new MongoClient(url);
 
-const dbName = 'greenmapper';
+const dbName = "greenmapper";
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 // Upload to db test
-app.get('/checkpolygon', async (req, res) => {
+app.get("/checkpolygon", async (req, res) => {
   try {
     let collection = await connectDb();
-    
-    const result = await collection.find({
-      location: {
-        $geoIntersects: {
-          $geometry: {
-            type: "Polygon",
-            coordinates: JSON.parse(req.query.polygon)
-          }
-        }
-      }
-    }).toArray();
+
+    const result = await collection
+      .find({
+        location: {
+          $geoIntersects: {
+            $geometry: {
+              type: "Polygon",
+              coordinates: JSON.parse(req.query.polygon),
+            },
+          },
+        },
+      })
+      .toArray();
 
     res.send(result.length > 0);
   } catch (error) {
@@ -41,21 +43,23 @@ app.get('/checkpolygon', async (req, res) => {
   }
 });
 
-// 
-app.get('/getIntersectsInGrid', async (req, res) => {  
+//
+app.get("/getIntersectsInGrid", async (req, res) => {
   try {
-    let collection = await connectDb('grid');
-    const result = await collection.find({
-      geometry: {
-        $geoIntersects: {
+    let collection = await connectDb("grid");
+    const result = await collection
+      .find({
+        geometry: {
+          $geoIntersects: {
             $geometry: {
-                type: 'Polygon',
-                coordinates: JSON.parse(req.query.polygon)
-            }
-        }
-    }
-    }).toArray();
-
+              type: "Polygon",
+              coordinates: JSON.parse(req.query.polygon),
+              },
+          },
+        },
+      })
+      .toArray();
+console.log(JSON.parse(req.query.polygon));
     res.send(result);
   } catch (error) {
     res.status(500).send(error.message);
@@ -63,49 +67,55 @@ app.get('/getIntersectsInGrid', async (req, res) => {
 });
 
 // Created the entire grid of 100*100 cells and stores it in mongodb
-app.get('/createGrid', async (req, res) => {
+app.get("/createGrid", async (req, res) => {
   try {
     let collection = await connectDb("grid");
 
     const size = await collection.countDocuments();
 
-    if(size == 0) {
-     // Define the bounding box for Trollhättan
-    const minLat = -90; // Minimum latitude
-    const maxLat = 90; // Maximum latitude
-    const minLon = -180; // Minimum longitude
-    const maxLon = 180; // Maximum longitude
+    if (size == 0) {
+      // Define the bounding box
+      const minLat = -90; // Minimum latitude
+      const maxLat = 90; // Maximum latitude
+      const minLon = -180; // Minimum longitude
+      const maxLon = 180; // Maximum longitude
 
-    // Define the size of each grid cell in meters
-    const cellSize = 1000000; // meters
+      // Define the size of each grid cell in meters
+      const cellSize = 10000;
 
-    // Calculate the number of steps needed for latitude and longitude
-    const numberOfStepsLat = Math.ceil((maxLat - minLat) * 111319.45 / cellSize);
-    const numberOfStepsLon = Math.ceil((maxLon - minLon) * 111319.45 * Math.cos(minLat * Math.PI / 180) / cellSize);
-    console.time('grid create');
-    // Iterate over latitude and longitude within the bounding box
-    for (let lat = minLat; lat < maxLat; lat += cellSize / 111319.45) {
-        for (let lon = minLon; lon < maxLon; lon += (cellSize / (111319.45 * Math.cos(lat * Math.PI / 180)))) {
-            const vertices = [
+      const vertices = [];
+      let latStepAmount;
+      let lonOffset;
+      let latOffset;
+      const degreesPerMeter = cellSize / 111319.45;
+      console.time("grid create");
+      // Iterate over latitude and longitude within the bounding box
+      for (let lat = minLat; lat < maxLat; lat += degreesPerMeter) {
+        latStepAmount =
+          cellSize / (111319.45 * Math.cos((lat * Math.PI) / 180));
+        latOffset = lat + degreesPerMeter;
+        for (let lon = minLon; lon < maxLon; lon += latStepAmount) {
+          lonOffset = lon + latStepAmount;
+          vertices.push({
+            _id: { lat, lon, cellSize },
+            type: "Feature",
+            geometry: {
+              type: "Polygon",
+              coordinates: [[
                 [lon, lat],
-                [lon + (cellSize / (111319.45 * Math.cos(lat * Math.PI / 180))), lat],
-                [lon + (cellSize / (111319.45 * Math.cos(lat * Math.PI / 180))), lat + (cellSize / 111319.45)],
-                [lon, lat + (cellSize / 111319.45)],
-                [lon, lat]  // Repeat the first vertex to close the polygon
-            ];
-
-            // Insert the polygon into MongoDB
-            await collection.insertOne({
-                type: 'Feature',
-                geometry: {
-                    type: 'Polygon',
-                    coordinates: [vertices]
-                }
-            });
+                [lonOffset, lat],
+                [lonOffset, latOffset],
+                [lon, latOffset],
+                [lon, lat],
+              ]],
+              },
+          });
         }
+      }
+      // Insert the polygon into MongoDB
+      await collection.insertMany(vertices);
+      console.timeEnd("grid create");
     }
-    console.timeEnd('grid create');
-  }
     res.send(size);
   } catch (error) {
     res.status(500).send(error.message);
@@ -113,10 +123,10 @@ app.get('/createGrid', async (req, res) => {
 });
 
 // Only for debug
-app.get('/getGrid', async (req, res) => {
+app.get("/getGrid", async (req, res) => {
   try {
-    let collection = await connectDb('grid');
-    
+    let collection = await connectDb("grid");
+
     const result = await collection.find().toArray();
     res.send(result);
   } catch (error) {
@@ -125,32 +135,37 @@ app.get('/getGrid', async (req, res) => {
 });
 
 // Upload to db test
-app.post('/sethome', async (req, res) => {
+app.post("/sethome", async (req, res) => {
   let collection = await connectDb();
- 
-  // collection.updateOne({}, { $set: {home: req.body.home} }, { upsert: true });
-  await collection.updateOne({}, { $set: {
-    location: { type: "Point", coordinates: req.body}
-  }}, { upsert: true });
 
-  res.send('home updated')
-})
+  // collection.updateOne({}, { $set: {home: req.body.home} }, { upsert: true });
+  await collection.updateOne(
+    {},
+    {
+      $set: {
+        location: { type: "Point", coordinates: req.body },
+      },
+    },
+    { upsert: true },
+  );
+
+  res.send("home updated");
+});
 // Read db test
-app.get('/gethome', async (req, res) => {
-  
+app.get("/gethome", async (req, res) => {
   let collection = await connectDb();
 
   let results = await collection.aggregate([]).toArray();
   res.send(results).status(200);
-})
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
 
 async function connectDb(collectionName = "test") {
   await client.connect();
-  console.log('Connected successfully to server');
+  console.log("Connected successfully to server");
   const db = client.db(dbName);
   return db.collection(collectionName);
 }
