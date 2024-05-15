479c85520662524f14553fd029e8f4c66d4d5084
Andreas Sanz <andreas.sanz.lerena@gmail.com>
2024-05-15 13:12:19 +0200

    MGRS

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const { MongoClient } = require("mongodb");
 const cors = require("cors");
 const bodyparser = require("body-parser");
 const axios = require("axios");
+const MGRS = require("@ngageoint/mgrs-js");
+const { Point } = require("@ngageoint/grid-js");
+require("@ngageoint/grid-js");
+
 
 require("dotenv").config();
 
@@ -30,36 +34,40 @@ app.get("/getIntersectsInGrid", async (req, res) => {
     const radius = Math.ceil(req.query.radius);
     const area = Number(req.query.area);
     let gridSize = 10000;
+    let mgrsSize = MGRS.GridType.TEN_KILOMETER;
     let collectionName = "grid-100km";
     let tempCollectionName = "";
-    console.log(area);
     // World
     if(area > Number(req.query.worldMinArea)) {
       gridSize = 100000;
+      // The frontend cant handle 100km size
+      mgrsSize = MGRS.GridType.TEN_KILOMETER;
       collectionName = "grid-100km";
     }
     // Country
     else if (area > Number(req.query.countryMinArea)) {
       gridSize = 10000;
+      mgrsSize = MGRS.GridType.TEN_KILOMETER;
       collectionName = "grid-10km";
     }
-    // region
+    // Region
     else if (area > Number(req.query.regionMinArea)) {
       gridSize = 1000;
-      collectionName = "grid-1km";
+      mgrsSize = MGRS.GridType.KILOMETER;
+      collectionName = "grid-1kmm";
     }
-    // neighbourhood
+    // Neighbourhood
     else if (area > 0) {
       gridSize = 100;
-      collectionName = "grid-1km";
+      mgrsSize = MGRS.GridType.HUNDRED_METER;
+      collectionName = "grid-1kmm";
       tempCollectionName = "grid-temp-" + Math.floor(Math.random() * 10000000).toString();
-      console.log(tempCollectionName);
     }
     else {
       res.status(500).send("Error: No polygon");
       return;
     }
-    
+    console.log("GRIDSIZE: ", gridSize);
     let collection = await connectDb(collectionName);
     const result = await collection
       .aggregate([{
@@ -69,39 +77,50 @@ app.get("/getIntersectsInGrid", async (req, res) => {
               maxDistance: radius * 1.4,
           },
       },
-    ])
-      .toArray();
+    ]).toArray();
       // If the grid is 100*100m, create a temporary grid based on 1km cells
       if(gridSize == 100) {
+        console.log("100m");
         for await (const [i, cell] of result.entries()) {
           let tempCollection = await connectDb(tempCollectionName);
           await createGrid(options = {
             collection: tempCollection,
             minLon: cell.geometry.coordinates[0],
-            maxLon: cell.offset.lon,
+            maxLon: cell.geometry.coordinates[0] + cell.offset.lon,
             minLat: cell.geometry.coordinates[1],
-            maxLat: cell.offset.lat,
+            maxLat: cell.geometry.coordinates[1] + cell.offset.lat,
             cellSize: 100,
             index: i
           });
         }
       }
       let intersectCollection = await connectDb(gridSize == 100 ? tempCollectionName : collectionName);
-        let intsersectionResult = await intersectCollection.find({
-          geometry: {
-            $geoIntersects: {
-              $geometry: {
-                type: "Polygon",
-                coordinates: polygon
-              }
-            }
+
+    let filter = {
+      geometry: {
+        $geoIntersects: {
+          $geometry: {
+            type: "Polygon",
+            coordinates: polygon
           }
-        }).toArray();
-        if(gridSize == 100) {
-          let tempCollection = await connectDb(tempCollectionName);
-          await tempCollection.drop();
         }
-    res.send(intsersectionResult);
+      }
+    };
+
+    let intsersectionResult = await intersectCollection.find(filter).toArray();
+    if(gridSize == 100) {
+      let tempCollection = await connectDb(tempCollectionName);
+      await tempCollection.drop();
+    }
+
+    let mgrsGrid = [];
+
+    for (cell of intsersectionResult) {
+      console.log(cell);
+      mgrsGrid.push(getMGRSStringFromLatLng(cell.geometry.coordinates[1], cell.geometry.coordinates[0], mgrsSize));
+    }
+    console.log(mgrsGrid);
+    res.send({createdGrid: intsersectionResult, mgrsGrid});
 
   } catch (error) {
     console.log(error.message);
@@ -109,7 +128,7 @@ app.get("/getIntersectsInGrid", async (req, res) => {
   }
 });
 
-// Created the entire grid of 100*100 cells and stores it in mongodb
+// Created the entire grid of size "cellSize" cells and stores it in mongodb
 app.get("/createGrid", async (req, res) => {
   try {
     let collection = await connectDb("grid-100km");
@@ -118,9 +137,14 @@ app.get("/createGrid", async (req, res) => {
 
     if (size == 0) {
       const result = await createGrid({
+        minLat: -90,
+        maxLat: 90,
+        minLon: -180,
+        maxLon: 180,
         collection: collection,
         cellSize: 100000
       });
+      await collection.createIndex( { "geometry" : "2dsphere" } );
       res.send({ created: result.length });
     }
   } catch (error) {
@@ -128,7 +152,7 @@ app.get("/createGrid", async (req, res) => {
   }
 });
 
-// Created the entire grid of 100*100 cells and stores it in mongodb
+// Find a cell based on longitude and latitude
 app.get("/cell", async (req, res) => {
   try {
     console.log(req.query);
@@ -151,6 +175,7 @@ app.get("/cell", async (req, res) => {
     res.status(500).send(error.message);
   }
 });
+
 // Search in GeoServer
 app.get("/geoServer", async (req, res) => {
   try {
@@ -229,9 +254,16 @@ app.post("/sethome", async (req, res) => {
 // Read db test
 app.get("/gethome", async (req, res) => {
   let collection = await connectDb();
-
+  let coords = [];
+  for (let x = 0; x < 10; x++) {
+    for (let y = 0; y < 10; y++) {
+      const mgrsObject = MGRS.MGRS.from(Point.point(x / 10, y / 10));
+      const mgrs10km = mgrsObject.coordinate(MGRS.GridType.TEN_KILOMETER);
+      coords.push(mgrs10km);
+    }
+  }
   let results = await collection.aggregate([]).toArray();
-  res.send(results).status(200);
+  res.send(coords).status(200);
 });
 
 app.listen(port, () => {
@@ -250,37 +282,34 @@ async function createGrid(options = { collection, minLat, maxLat, minLon, maxLon
 
   let vertices = [];
   let lonStepAmount;
-  let lonOffset;
-  let latOffset;
 
   let latStepAmount = Math.abs(minLat - calculateDegreesLatitude(minLat, cellSize));
-  // console.log(latStepAmount);
   lonStepAmount = Math.abs(calculateDegreesLongitude(minLon, cellSize, minLat, minLat + latStepAmount));
   let lastLon = minLon;
   
-  for (let lat = minLat + latStepAmount / 2; lat + latStepAmount < maxLat - latStepAmount; lat += latStepAmount) {
-    latStepAmount = Math.abs(lat - calculateDegreesLatitude(lat, cellSize, 180));
+  for (let lat = minLat; lat + latStepAmount < maxLat + 0.0001; lat += latStepAmount) {
+    latStepAmount = Math.abs(lat - calculateDegreesLatitude(lat, cellSize, 0));
     // λ2 = λ1 + atan2( sin θ ⋅ sin δ ⋅ cos φ1, cos δ − sin φ1 ⋅ sin φ2 )
-    latOffset = Math.abs(lat - latStepAmount);
     lonStepAmount = Math.abs(calculateDegreesLongitude(lastLon, cellSize, lat, lat + latStepAmount));
-    for (let lon = minLon; lon + lonStepAmount < maxLon; lon += lonStepAmount) {
+    for (let lon = minLon; maxLon > lon + lonStepAmount - 0.00001; lon += lonStepAmount) {
       lastLon = lon;
-      lonOffset = Math.abs(lon - lonStepAmount);
       let vertice = {
         _id: { lat, lon, cellSize },
         geometry: {
           type: "Point",
-          coordinates: [((lon + (lonStepAmount / 2) + 180) % 360 + 360) % 360 - 180, lat + (latStepAmount / 2)],
+          coordinates: [((lon + 180) % 360 + 360) % 360 - 180, lat],
         },
         offset: {
-          lon: lonStepAmount / 2,
-          lat: latStepAmount / 2
+          lon: lonStepAmount,
+          lat: latStepAmount
         }
       };
       if (options.index != null) {
         vertice._id.index = options.index;
       }
-      vertices.push(vertice);
+      // if (vertices.length == 0) {
+        vertices.push(vertice);
+      // }
     }
     if (collection != null && vertices.length > 2000000) {
       console.log("Insert: ", vertices.length);
@@ -290,10 +319,12 @@ async function createGrid(options = { collection, minLat, maxLat, minLon, maxLon
     }
   }
   if(collection != null) {
+    console.log("insert", vertices);
     await collection.insertMany(vertices, { ordered: false });
   }
   // Insert the polygon into MongoDB
   // console.timeEnd("grid create");
+  console.log("vertices", vertices);
   return vertices;
 }
 
@@ -301,7 +332,8 @@ function calculateDegreesLatitude(lat1, distance, bearing = 90) {
   const lat1Rad = lat1 * (Math.PI / 180); // Convert latitude to radians
   const bearingRad = bearing * (Math.PI / 180); // Convert bearing to radians
 
-  return Math.asin(Math.sin(lat1Rad) * Math.cos(distance / earthRadius) + Math.cos(lat1Rad) * Math.sin(distance / earthRadius) * Math.cos(bearingRad)) * (180 / Math.PI);
+  return Math.asin(Math.sin(lat1Rad) * Math.cos(distance / earthRadius) + Math.cos(lat1Rad) * 
+  Math.sin(distance / earthRadius) * Math.cos(bearingRad)) * (180 / Math.PI);
 }
 
 function calculateDegreesLongitude(lon1, distance, lat1, lat2, bearing = 90) {
@@ -311,6 +343,11 @@ function calculateDegreesLongitude(lon1, distance, lat1, lat2, bearing = 90) {
 
   return Math.atan2(Math.sin(bearingRad) * Math.sin(distance / earthRadius) * Math.cos(lat1Rad),
       Math.cos(distance / earthRadius) - Math.sin(lat1Rad) * Math.sin(lat2Rad)) * (180 / Math.PI);
+}
+
+function getMGRSStringFromLatLng(lat, lng, size) {
+  const mgrsObject = MGRS.MGRS.from(Point.point(lng, lat));
+  return mgrsObject.coordinate(size);
 }
 
 async function connectDb(collectionName = "test") {
